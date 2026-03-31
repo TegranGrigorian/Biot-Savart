89a1af1521f4897a300e69f1fdf1a9a79e783a40
TegranGrigorian <tegrgrigor@gmail.com>
2026-03-31 17:06:12 -0400

    test cross product with the hw problem

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ mod constants;
 use engine::components::wire::Wire;
 use engine::components::point::Point;
 fn main() {
-    let wire1: Wire = Wire {name: String::from("W1"), points: vec![Vector3::new(0.0,0.0,0.0), Vector3::new(1.0,2.0,3.6)]};
-    let point: Point = Point { name: String::from("P1"), cords: Vector3::new(0.0, 23.3, 21.1) };
-
+    let wire1: Wire = Wire {name: String::from("W1"), points: vec![Vector3::new(1.0,0.0,0.0), Vector3::new(1.0,2.0,3.6)]};
+    let point: Point = Point { name: String::from("P1"), cords: Vector3::new(-1.0, 1.0, 0.0) };
     println!("Wire {} points {:?}", wire1.name, wire1.points);
-    println!("Test cross product: {:?}",  wire1.points[1].cross(&point.cords));
+    println!("Test cross product: {:?}",  wire1.points[0].cross(&point.cords));
     println!("Mu0 {}", constants::MU_0);
     println!("Point Name: {} at {:?}", point.name, point.cords);
 }
