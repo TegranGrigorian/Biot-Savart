6394a576917a659771de63f4dbb9021a87118b79
TegranGrigorian <tegrgrigor@gmail.com>
2026-04-01 15:56:33 -0400

    consts commenting

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,6 @@
 use std::f32::consts::PI;
+
+//consts
 const MU_0: f32 = 4.0 * PI * 1.0e-7; // 4pi * 10^-7 Tm / A
 pub static K:f32 = MU_0 / (4.0 * std::f32::consts::PI);
 pub const DIVIDER: i16 = 10000; // how many "dB"'s we will be calculating
