1ae46a12b0fa023a19ccf389a38625b46c20bb96
TegranGrigorian <tegrgrigor@gmail.com>
2026-04-01 15:56:04 -0400

    added comments to higher level files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use Biot_Savart::{test};
+// use Biot_Savart::test::test_biot_savart;
 mod constants;
-use Biot_Savart::app::screen::run_viewer;
+use Biot_Savart::{app::screen::run_viewer};
 fn main() {
-    run_viewer();
-    // test::test_biot_savart();
+    run_viewer(); // view /app/viewer.rs for this code
+    // test_biot_savart();
 }
--- src/test.rs
+++ src/test.rs
@@ -2,17 +2,30 @@ use crate::engine::components::point::Point;
 use crate::engine::components::wire::Wire;
 use nalgebra::Vector3;
 use crate::engine::math::Math;
+/*
+Test Engine with pre computed answer
+Probe: (0, 1, 0)
+Wire: (0,0,0) -> (0.01, 0, 0) @ 2 Amps
+Ans = (0i + 0j + 2.0 * 10^-9 K)T
+*/
 pub fn test_biot_savart() -> Result<(), ()> {
+    // wire
     let wire: Wire = Wire::new(String::from("Wire 1"), vec![
         Vector3::new(0.0, 0.0, 0.0), 
         Vector3::new(0.01, 0.0, 0.0)]);
+    
+    // point
     let point: Point = Point::new(String::from("P1"), 
     Vector3::new(0.0, 1.0, 0.0));
+
+    // output + check if output is in range of what it should be
     let output = Math::calculate_biot_savart(wire, point, 2.0).unwrap();
     if !(&output <= &(2.0e-9 + 0.5e-9) && (&output >= &(2.0e-9 - 0.5e-9))) {
         println!("NOT RIGHT VALUE");
         return Err(());
     }
+    
+    // print debug
     println!("GOOD, VALUE: {:.3e}", output);
     Ok(())
 }
