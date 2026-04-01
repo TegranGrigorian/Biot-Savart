503a7b393396d1f94b6150cb538fd55f619881d3
TegranGrigorian <tegrgrigor@gmail.com>
2026-04-01 13:46:26 -0400

    logic for wires and points.

--- src/app/screen.rs
+++ src/app/screen.rs
@@ -1,17 +1,24 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiPrimaryContextPass};
+use nalgebra::Vector3;
 use crate::engine::components::wire::Wire;
 use crate::engine::components::point::Point;
+use crate::engine::math::Math;
+
 #[derive(Resource, Default)]
 struct UiState {
     current: f32,
     wire_x: f32,
     wire_y: f32,
     wire_z: f32,
+    wire_points: Vec<Vector3<f32>>,
     probe_x: f32,
     probe_y: f32,
     probe_z: f32,
+    last_b: Option<f32>,
+    last_error: Option<String>,
     add_wire_point_clicked: bool,
+    clear_wire_clicked: bool,
     set_probe_clicked: bool,
 }
 
@@ -80,6 +87,10 @@ fn ui_panel_system(mut contexts: EguiContexts, mut ui_state: ResMut<UiState>) {
         if ui.button("Add wire point").clicked() {
             ui_state.add_wire_point_clicked = true;
         }
+        if ui.button("Clear wire").clicked() {
+            ui_state.clear_wire_clicked = true;
+        }
+        ui.label(format!("Wire points: {}", ui_state.wire_points.len()));
 
         ui.separator();
         ui.label("Probe point");
@@ -91,21 +102,53 @@ fn ui_panel_system(mut contexts: EguiContexts, mut ui_state: ResMut<UiState>) {
         if ui.button("Set probe").clicked() {
             ui_state.set_probe_clicked = true;
         }
+
+        ui.separator();
+        if let Some(b) = ui_state.last_b {
+            ui.label(format!("|B| = {:.3e} T", b));
+        }
+        if let Some(err) = &ui_state.last_error {
+            ui.colored_label(egui::Color32::RED, err);
+        }
     });
 }
 
 fn apply_ui_actions_system(mut ui_state: ResMut<UiState>) {
+    if ui_state.clear_wire_clicked {
+        ui_state.wire_points.clear();
+        ui_state.last_b = None;
+        ui_state.last_error = None;
+        info!("Cleared wire points");
+        ui_state.clear_wire_clicked = false;
+    }
+
     if ui_state.add_wire_point_clicked {
-        let p = Vec3::new(ui_state.wire_x, ui_state.wire_y, ui_state.wire_z);
-        info!("Add wire point: {:?}", p);
-        // TODO: call your engine to append this point to the wire
+        let p = Vector3::new(ui_state.wire_x, ui_state.wire_y, ui_state.wire_z);
+        ui_state.wire_points.push(p);
+        info!("Added wire point: [{:.3}, {:.3}, {:.3}]", p.x, p.y, p.z);
         ui_state.add_wire_point_clicked = false;
     }
 
     if ui_state.set_probe_clicked {
-        let p = Vec3::new(ui_state.probe_x, ui_state.probe_y, ui_state.probe_z);
-        info!("Set probe point: {:?}", p);
-        // TODO: call your engine to set probe point and compute B
+        let wire = Wire::new(String::from("Wire 1"), ui_state.wire_points.clone());
+        let point = Point::new(
+            String::from("Probe"),
+            Vector3::new(ui_state.probe_x, ui_state.probe_y, ui_state.probe_z),
+        );
+
+        match Math::calculate_biot_savart(wire, point, ui_state.current) {
+            Ok(b) => {
+                ui_state.last_b = Some(b);
+                ui_state.last_error = None;
+                info!("Computed |B| = {:.3e} T", b);
+            }
+            Err(err) => {
+                ui_state.last_b = None;
+                ui_state.last_error = Some(err.clone());
+                info!("Biot-Savart compute error: {}", err);
+            }
+        }
+
         ui_state.set_probe_clicked = false;
     }
 }
