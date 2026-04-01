aeaeae46d1b2818ca3312ac57f0a4601a183faf5
TegranGrigorian <tegrgrigor@gmail.com>
2026-03-31 22:21:41 -0400

    new new mthod in points. what does this even mean. new new?/

--- src/engine/components/point.rs
+++ src/engine/components/point.rs
@@ -4,3 +4,9 @@ pub struct Point {
     pub name: String,
     pub cords: Vector3<f32>
 }
+
+impl Point {
+    pub fn new(name: String, cords: Vector3<f32>) -> Point {
+        Point {name: name, cords: cords}
+    }
+}
--- src/engine/math.rs
+++ src/engine/math.rs
@@ -57,17 +57,10 @@ impl Math {
     }
 
     pub fn test_calc_bs() {
-        // let wire = Wire {name: String::from("Wire 1"), 
-        // points: vec![
-        //     Vector3::new(0.0, 0.0, 0.0), 
-        //     Vector3::new(0.01, 0.0, 0.0)] 
-        // };
-          let wire: Wire = Wire::new(String::from("Wire 1"), vec![
+        let wire: Wire = Wire::new(String::from("Wire 1"), vec![
             Vector3::new(0.0, 0.0, 0.0), 
             Vector3::new(0.01, 0.0, 0.0)]);
-        let point: Point = Point {name: String::from("p1"), 
-        cords: Vector3::new(0.0, 1.0, 0.0)
-        };
+        let point: Point = Point::new(String::from("P1"), Vector3::new(0.0, 1.0, 0.0));
         let output = Math::calculate_biot_savart(wire, point, 2.0);
         println!("Output: {:.2e}", &output.unwrap());
     }
