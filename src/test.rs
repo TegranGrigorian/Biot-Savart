798df6598833d8f96d9ab54b5acfd9b972a674ec
TegranGrigorian <tegrgrigor@gmail.com>
2026-03-31 22:33:20 -0400

    use more line

--- src/test.rs
+++ src/test.rs
@@ -6,7 +6,8 @@ pub fn test_biot_savart() -> Result<(), ()> {
     let wire: Wire = Wire::new(String::from("Wire 1"), vec![
         Vector3::new(0.0, 0.0, 0.0), 
         Vector3::new(0.01, 0.0, 0.0)]);
-    let point: Point = Point::new(String::from("P1"), Vector3::new(0.0, 1.0, 0.0));
+    let point: Point = Point::new(String::from("P1"), 
+    Vector3::new(0.0, 1.0, 0.0));
     let output = Math::calculate_biot_savart(wire, point, 2.0).unwrap();
     if !(&output <= &(2.0e-9 + 0.5e-9) && (&output >= &(2.0e-9 - 0.5e-9))) {
         println!("NOT RIGHT VALUE");
